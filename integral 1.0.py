a86b434520a9c2d6d59e140d507e750baf720779
grishasolakhyan <borsch.tv@mail.ru>
2023-03-08 00:54:30 +1000

    Hello, World!

--- integral 1.0.py
+++ integral 1.0.py
@@ -8,7 +8,9 @@ from matplotlib.figure import Figure # Фигура для черчения
 
 # Импортирование необходимых виджетов
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMessageBox, QLabel, QWidget, QApplication, QPushButton, QMainWindow, QHBoxLayout, QVBoxLayout, QTextEdit
+from PyQt5.QtWidgets import QMessageBox, QLabel, QSlider, QWidget, QApplication, QPushButton, QMainWindow, QHBoxLayout, QVBoxLayout, QTextEdit
+from PyQt5.QtCore import QRect, Qt
+from PyQt5.QtGui import QTextOption
 
 class PlotWidget(QWidget):
     def __init__(self, parent=None):
@@ -59,17 +61,60 @@ class MainWindow(QMainWindow):
         self.btn_graph = QPushButton('Построить график')
         self.btn_integral = QPushButton('Вычислить')
 
-        self.equation = QTextEdit()
+        self.equation = QTextEdit(self)
+        self.equation.setFixedSize(150, 24)
+        self.equation.setWordWrapMode(QTextOption.NoWrap)
 
+        self.tit_equation = QLabel('Уравнение f(x)')
 
-        self.btn_graph.setStyleSheet('font-size: 12pt; font-weight: 530;')
-        self.btn_integral.setStyleSheet('font-size: 12pt; font-weight: 530;')
+        self.Xa = QTextEdit()
+        self.Xa.setFixedSize(150, 24)
+
+        self.tit_Xa = QLabel('Левая граница')
+
+        self.Xb = QTextEdit()
+        self.Xb.setFixedSize(150, 24)
+
+        self.tit_Xb = QLabel('Правая граница')
+
+        self.num_seg = QSlider()
+        self.num_seg.setOrientation(Qt.Horizontal)
+
+        self.tit_num_seg = QLabel('Число разбиений')
+        self.text_num_seg = QLabel('1')
+
+        self.res_rect = QTextEdit()
+        self.res_rect.setFixedSize(150, 24)
+
+        self.tit_res_rect = QLabel('Метод прямоугольников')
+
+        self.res_trap = QTextEdit()
+        self.res_trap.setFixedSize(150, 24)
+
+        self.tit_res_trap = QLabel('Метод трапеций')
+
+        self.res_Simp = QTextEdit()
+        self.res_Simp.setFixedSize(150, 24)
+
+        self.tit_res_Simp = QLabel('Метод Симпсона')
+
+        #self.btn_graph.setStyleSheet('font-size: 12pt; font-weight: 530;')
+        #self.btn_integral.setStyleSheet('font-size: 12pt; font-weight: 530;')
 
         #self.bl.addWidget(self.buildButton)
         #self.bl.addWidget(self.calculateButton)
 
         #self.l.addLayout(self.bl)
         #self.l.addWidget(self.plotWidget)
+
+        self.H6.addWidget(self.res_rect)
+        self.H6.addWidget(self.tit_res_rect)
+
+        self.H7.addWidget(self.res_trap)
+        self.H7.addWidget(self.tit_res_trap)
+
+        self.H8.addWidget(self.res_Simp)
+        self.H8.addWidget(self.tit_res_Simp)
 
         self.V7.addLayout(self.H6)
         self.V7.addLayout(self.H7)
@@ -78,14 +123,27 @@ class MainWindow(QMainWindow):
         self.V6.addWidget(self.btn_graph)
         self.V6.addWidget(self.btn_integral)
 
-        self.V3.addLayout(self.V7)
         self.V3.addLayout(self.V6)
+        self.V3.addLayout(self.V7)
+
+        self.H2.addWidget(self.equation)
+        self.H2.addWidget(self.tit_equation)
+
+        self.H3.addWidget(self.Xa)
+        self.H3.addWidget(self.tit_Xa)
+
+        self.H4.addWidget(self.Xb)
+        self.H4.addWidget(self.tit_Xb)
 
         self.V4.addLayout(self.H2)
         self.V4.addLayout(self.H3)
         self.V4.addLayout(self.H4)
 
+        self.H5.addWidget(self.num_seg)
+        self.H5.addWidget(self.text_num_seg)
+
         self.V5.addLayout(self.H5)
+        self.V5.addWidget(self.tit_num_seg)
 
         self.V2.addLayout(self.V4)
         self.V2.addLayout(self.V5)
