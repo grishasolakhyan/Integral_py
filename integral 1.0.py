91ec711c10698952727f571c3ebfc5729019b44d
grishasolakhyan <borsch.tv@mail.ru>
2023-03-23 18:21:01 +1000

    Hello, World!

--- integral 1.0.py
+++ integral 1.0.py
@@ -2,6 +2,7 @@ import sys
 import random
 import numpy as np
 
+from matplotlib import pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas # Область для черчения
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar # Панель управления
 from matplotlib.figure import Figure # Фигура для черчения
@@ -11,80 +12,6 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMessageBox, QLabel, QSlider, QWidget, QApplication, QPushButton, QMainWindow, QHBoxLayout, QVBoxLayout, QTextEdit, QGroupBox
 from PyQt5.QtCore import QRect, Qt
 from PyQt5.QtGui import QTextOption
-
-
-class Methods():
-    def __init__(self, parent=None):
-        super(Methods, self).__init__()
-    def hi(self):
-        print("HI!")
-    '''
-    def rectangle(self): #функция метода прямоугольников
-        xa, xb = self.int_borders()
-        n = self.update_NumSeg()
-        sum = 0
-        h = (xb - xa) / n
-        btx = xa + h/2
-        while(btx < xb):
-            sum += self.func(btx)
-            btx += h
-        return sum * h
-
-    def trapezium(self): #функция метода трапеций
-        xa, xb = self.int_borders()
-        n = self.update_NumSeg()
-        sum = 0
-        h = (xb - xa) / n
-        btx = xa+h
-        sum = (self.func(xa)+self.func(xb))/2
-        for i in range (1, n):
-            sum += self.func(btx)
-            btx += h
-        return sum * h
-
-    def Simpson(self): #функция метода Симпсона
-        xa, xb = self.int_borders()
-        n = self.update_NumSeg()
-        sum=0
-        btx = xa
-        h = (xb - xa) / n
-        sum = self.func(xa)+self.func(xb)
-        sum1 = 0
-        sum2 = 0
-        for i in range (1, n, 2):
-            sum1 += self.func(btx + i * h)
-        for i in range (2, n, 2):
-            sum2 += self.func(btx + i * h)
-        sum = (h / 3) * (sum + 4 * sum1 + 2 * sum2)
-        return sum
-    '''
-
-
-class PlotWidget(QWidget, Methods):
-    def __init__(self, parent=None):
-        super(PlotWidget, self).__init__(parent) # Инициализирование экземпляр
-        self.initUi() # Формирование интерфейса
-
-    def initUi(self):
-        self.mainLayout = QVBoxLayout(self)
-
-        self.figure = Figure()
-        self.canvas = FigureCanvas(self.figure)
-        self.navToolbar = NavigationToolbar(self.canvas, self)
-        self.mainLayout.addWidget(self.canvas)
-        self.mainLayout.addWidget(self.navToolbar)
-
-    def Integral(self):
-        hihi = self.hi()
-        '''
-        r_res = self.rectangle()
-        t_res = self.trapezium()
-        s_res = self.Simpson()
-        print("Hello, World!")
-        '''
-
-    def Graph(self):
-        print("Hello, Human!")
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -96,6 +23,9 @@ class MainWindow(QMainWindow):
 
     def initUi(self):
         self.centralWidget = QWidget(self)
+        self.plotWidget = PlotWidget()
+        self.integral_methods = Integral_Methods(self)
+
         self.H1 = QHBoxLayout(self.centralWidget)
         self.H2 = QHBoxLayout(self.centralWidget)
         self.H3 = QHBoxLayout(self.centralWidget)
@@ -112,8 +42,6 @@ class MainWindow(QMainWindow):
         self.V5 = QVBoxLayout(self.centralWidget)
         self.V6 = QVBoxLayout(self.centralWidget)
         self.V7 = QVBoxLayout(self.centralWidget)
-
-        self.plotWidget = PlotWidget()
 
         self.btn_graph = QPushButton('Построить график')
         self.btn_graph.setFixedWidth(150)
@@ -219,9 +147,43 @@ class MainWindow(QMainWindow):
         return val
 
     def connectUi(self):
-        self.btn_integral.clicked.connect(self.plotWidget.Integral)
+        self.btn_integral.clicked.connect(self.integral_methods.int_borders)
         self.btn_graph.clicked.connect(self.plotWidget.Graph)
         self.num_seg.valueChanged.connect(self.update_NumSeg)
+
+class Integral_Methods():
+    def __init__(self, MainWindow, parent=None):
+        super(Integral_Methods, self).__init__()
+        self.mainwindow = MainWindow
+
+    def int_borders(self): #функция границ интеграла
+        self.xa = self.mainwindow.Xa
+        self.xb = self.mainwindow.Xb
+
+        self.xa = float(self.xa.toPlainText())
+        self.xb = float(self.xb.toPlainText())
+
+        print(self.xa, " ", self.xb)
+
+    def Integral(self):
+        print("Hello, World!")
+
+class PlotWidget(QWidget):
+    def __init__(self, parent=None):
+        super(PlotWidget, self).__init__(parent) # Инициализирование экземпляр
+        self.initUi() # Формирование интерфейса
+
+    def initUi(self):
+        self.mainLayout = QVBoxLayout(self)
+
+        self.figure = Figure()
+        self.canvas = FigureCanvas(self.figure)
+        self.navToolbar = NavigationToolbar(self.canvas, self)
+        self.mainLayout.addWidget(self.canvas)
+        self.mainLayout.addWidget(self.navToolbar)
+
+    def Graph(self):
+        print("Hello, Human!")
 
 app = QApplication([])
 p = MainWindow()
